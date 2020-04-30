1d30d762c11fc9b11365c957dd1fd2dc18305657
Kifah Meeran <contact@maskys.com>
2020-04-30 14:09:58 +0400

    Initial commit.

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const igClientId = "YOURCLIENTIDHERE";
+  static const igClientSecret = "YOURCLIENTSECRETHERE";
+  static const igRedirectURI = "https://YOURLINKHERE.com/";
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_basic_display_api_flutter/constants.dart';
+import 'package:simple_auth/simple_auth.dart' as simpleAuth;
+import 'package:simple_auth_flutter/simple_auth_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IG Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _errorMsg;
+  Map _userData;
+
+  final simpleAuth.InstagramApi _igApi = simpleAuth.InstagramApi(
+    "instagram",
+    Constants.igClientId,
+    Constants.igClientSecret,
+    Constants.igRedirectURI,
+    scopes: [
+      'user_profile', // For getting username, account type, etc.
+      'user_media', // For accessing media count & data like posts, videos etc.
+    ],
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    SimpleAuthFlutter.init(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Instagram Basic Display API Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Visibility(
+              visible: _userData != null,
+              child: Text(
+                _userData.keys.fold(
+                    '', (kvText, key) => kvText + "$key: ${_userData[key]} \n"),
+                textAlign: TextAlign.center,
+              ),
+              replacement:
+                  Text("Click the button below to get Instagram Login."),
+            ),
+            Visibility(
+              visible: _errorMsg != null,
+              child: Text("Error occured: $_errorMsg"),
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.input),
+              label: Text("Get Profile Data", ),
+              onPressed: _loginAndGetData,
+              color: Colors.pink.shade400,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _loginAndGetData() {
+    _igApi.authenticate().then(
+      (simpleAuth.Account _user) async {
+        simpleAuth.OAuthAccount user = _user;
+
+        var igUserResponse =
+            await Dio(BaseOptions(baseUrl: 'https://graph.instagram.com')).get(
+          '/me',
+          queryParameters: {
+            // Get the fields you need.
+            // https://developers.facebook.com/docs/instagram-basic-display-api/reference/user
+            "fields": "username,id,account_type,media_count",
+            "access_token": user.token,
+          },
+        );
+
+        setState(() {
+          _userData = igUserResponse.data;
+          _errorMsg = null;
+        });
+      },
+    ).catchError(
+      (Object e) {
+        setState(() => _errorMsg = e.toString());
+      },
+    );
+  }
+}
